dbcf9c23cb459a397e407160e88eda82f1d0c1d0
Jayant Shekhar <jayantshekhar@Jayants-iMac.local>
2015-10-02 23:57:30 -0700

    def added (function)

--- src/prime.py
+++ src/prime.py
@@ -2,24 +2,23 @@ __author__ = 'ansha'
 
 import sys
 
-n = 37 # find if n is prime
-print n
-i = 2  # start loop with 2
-print i
+def prime(n) : # n is an argument. find if n is prime or not
+    i = 2  # start loop with 2
+    while i < n:
+
+        print "i =", i
+        if n % i == 0: # mod
+            print n, "is not prime"
+            sys.exit() # stop script
+
+        i = i + 1 # adding 1 to i
+
+    print n, "is prime"
 
 
-while i < n:
-
-    print "i =", i
-    if n % i == 0: # mod
-        print n, "is not prime"
-        sys.exit() # stop script
-
-    i = i + 1 # adding 1 to i
-
-print n, "is prime"
-
-
+prime(7)  # find if 7 is prime
+prime(6)  # find if 6 is prime
+prime(11) # find if 11 is primt
 
 
 
