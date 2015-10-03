2decb942f672bf6e9b74f398129307bd696f0d2a
Jayant Shekhar <jayantshekhar@Jayants-iMac.local>
2015-10-02 23:49:16 -0700

    prime

--- src/prime.py
+++ src/prime.py
@@ -1,5 +1,28 @@
 __author__ = 'ansha'
 
+import sys
+
+n = 37 # find if n is prime
+print n
+i = 2  # start loop with 2
+print i
+
+
+while i < n:
+
+    print "i =", i
+    if n % i == 0: # mod
+        print n, "is not prime"
+        sys.exit() # stop script
+
+    i = i + 1 # adding 1 to i
+
+print n, "is prime"
+
+
+
+
+
 
 
 
